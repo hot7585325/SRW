68c5b72fc717f42b152d03393dcf3b23479a6636
hot7585325 <hot7585325@gmail.com>
2025-10-16 22:03:48 +0800

    增加左側panel動畫

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-const mainBtn=document.querySelector("#mainBtn");
-const left_btn=document.querySelector("#left_btn");
+const mainBtn = document.querySelector("#mainBtn");
+const left_panel = document.getElementById("left_panel");
 
-mainBtn.addEventListener("click",()=>{left_btn.display.width="20%"});
+let left_panel_Toggle = false;
+
+
+mainBtn.addEventListener("click", () => {
+
+    SwitchAni_left_panel(left_panel)
+});
+
+
+
+function SwitchAni_left_panel(DOM) {
+    left_panel_Toggle = !left_panel_Toggle
+    if (left_panel_Toggle) {
+        DOM.style.animation = "none";
+        void DOM.offsetWidth;
+        DOM.style.animation = "ani_left_panel 1s ease forwards";
+    } else {
+        DOM.style.animation = "none";
+        void DOM.offsetWidth;
+        DOM.style.animation = "ani_left_panel 1s ease forwards reverse";
+    }
+}
